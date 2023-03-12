71d1d0372d4d7f6ac3c48e5695d96276e4b3f430
IlyasMukatdisov <mukatdisovilyas@gmail.com>
2023-03-12 11:18:53 +0500

    created firebase project and fixed card

--- lib/providers/currency_provider.dart
+++ lib/providers/currency_provider.dart
@@ -1,6 +1,9 @@
 import 'package:currency_converter/utils/strings.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+
+
+
 final currencyAmountProvider =
     NotifierProvider<CurrencyAmountNotifier, Map<String, double>>(
   () {
@@ -47,36 +50,31 @@ class CurrencyTypeNotifier extends Notifier<Map<String, String>> {
   @override
   build() {
     return {
-      Strings.amountCurrency: Strings.uzs,
-      Strings.convertedAmountCurrency: Strings.usd,
+      Strings.amountCurrency: Strings.usd,
+      Strings.convertedAmountCurrency: Strings.eur,
     };
   }
 
   void setAmountCurrency(String amountCurrency) {
     if (amountCurrency == state[Strings.convertedAmountCurrency]) {
       state = {
-        Strings.amountCurrency:
-            state[Strings.convertedAmountCurrency] ?? Strings.uzs,
-        Strings.convertedAmountCurrency:
-            state[Strings.amountCurrency] ?? Strings.usd,
+        Strings.amountCurrency: state[Strings.convertedAmountCurrency]!,
+        Strings.convertedAmountCurrency: state[Strings.amountCurrency]!,
       };
       return;
     }
 
     state = {
       Strings.amountCurrency: amountCurrency,
-      Strings.convertedAmountCurrency:
-          state[Strings.convertedAmountCurrency]!,
+      Strings.convertedAmountCurrency: state[Strings.convertedAmountCurrency]!,
     };
   }
 
   void setConvertedAmountCurrency(String convertedAmountCurrency) {
     if (convertedAmountCurrency == state[Strings.amountCurrency]) {
       state = {
-        Strings.amountCurrency:
-            state[Strings.convertedAmountCurrency] ?? Strings.uzs,
-        Strings.convertedAmountCurrency:
-            state[Strings.amountCurrency] ?? Strings.usd,
+        Strings.amountCurrency: state[Strings.convertedAmountCurrency]!,
+        Strings.convertedAmountCurrency: state[Strings.amountCurrency]!,
       };
       return;
     }
